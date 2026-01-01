4cdba94e44783326b53fb724a86edafc05a3c2f2
Tu Pham <phamanhtu13121991@gmail.com>
2026-01-01 23:05:44 +0700

    feat: add subtitle to trigger CICD

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React Testing CICD - github actions</h1>
+      <h2>My First CI/CD Pipeline</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
